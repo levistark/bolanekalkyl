642515664866e1f2559036175bf1128542c85101
Levi Stark <levi.john.stark@gmail.com>
2023-11-02 14:23:04 +0100

    added a more accurate calculation of the moneyLeft and added the House/Apartment parameter into the validation function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ function getResult() {
     const housePrice = parseFloat(document.querySelector('#inp-housePrice').value)
     const deposit = parseFloat(document.querySelector('#inp-deposit').value)
     const interest = parseFloat(document.querySelector('#inp-interest').value)/100
-    const income = parseFloat(document.querySelector('#inp-income').value)
+    const income = parseFloat(document.querySelector('#inp-income').value)*0.66
     const opCosts = parseFloat(document.querySelector('#inp-opCosts').value)
     const otherCosts = parseFloat(document.querySelector('#inp-otherCosts').value)
     const amortizationPerYear = parseFloat(document.querySelector('#inp-amortization').value)/100
@@ -20,10 +20,37 @@ function getResult() {
     moneyLeftResult.textContent = moneyLeft.toFixed(1) + ' SEK'
 }
 
+
+
 function validateInputFields() {
     const errorMessage = "Du måste fylla i ett värde"
-    const inputFields = document.querySelectorAll('input')
+    const inputFields = document.querySelectorAll('input[type=\'number\']')
+    const checkBoxes = document.querySelectorAll('input[type=\'checkbox\']');
+    
     let results = []
+    let checkBoxArray = []
+    
+
+    for (checkBox of checkBoxes) {
+        if (checkBox.checked) {
+            checkBox = true
+            checkBoxArray.push(checkBox)
+
+        } else {
+            checkBox = false
+            checkBoxArray.push(checkBox)
+        }
+    }
+    
+    const areAllFalse = checkBoxArray.every(element => element === false);
+
+    if (areAllFalse) {
+        document.querySelector('.radio-text').textContent = errorMessage
+    } else {
+        document.querySelector('.radio-text').textContent = ''
+    }
+
+
 
     for (input of inputFields) {
         if (input.value === '') {
@@ -31,13 +58,15 @@ function validateInputFields() {
             input.previousElementSibling.textContent = errorMessage
             input = false
             results.push(input)
+            
         } else {
             input.classList.remove('warning')
             input.previousElementSibling.textContent = ''
         }
     }
 
-    if (!results.includes(false)) {
+    if (!results.includes(false) && checkBoxArray.includes(true)) {
         getResult()
     }
 }
+
