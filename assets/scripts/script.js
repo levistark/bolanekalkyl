505dbf7ec0a61485c3976183bf4013416179b940
Levi Stark <levi.john.stark@gmail.com>
2023-11-02 11:39:27 +0100

    added responsivity, and validation for the input fields

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,43 @@
-const btnResult = document.querySelector('.btn-result');
-
-btnResult.addEventListener('click', function getResult() {
+function getResult() {
     
-    const housePrice = parseFloat(document.querySelector('#inp-housePrice').value);
-    const deposit = parseFloat(document.querySelector('#inp-deposit').value);
-    const interest = parseFloat(document.querySelector('#inp-interest').value)/100;
-    const income = parseFloat(document.querySelector('#inp-income').value);
-    const opCosts = parseFloat(document.querySelector('#inp-opCosts').value);
-    const otherCosts = parseFloat(document.querySelector('#inp-otherCosts').value);
-    const amortizationPerYear = parseFloat(document.querySelector('#inp-amortization').value)/100;
-    const totalCostResult = document.querySelector('#li-totalCost');
-    const moneyLeftResult = document.querySelector('#li-moneyLeft');
+    const housePrice = parseFloat(document.querySelector('#inp-housePrice').value)
+    const deposit = parseFloat(document.querySelector('#inp-deposit').value)
+    const interest = parseFloat(document.querySelector('#inp-interest').value)/100
+    const income = parseFloat(document.querySelector('#inp-income').value)
+    const opCosts = parseFloat(document.querySelector('#inp-opCosts').value)
+    const otherCosts = parseFloat(document.querySelector('#inp-otherCosts').value)
+    const amortizationPerYear = parseFloat(document.querySelector('#inp-amortization').value)/100
+    const totalCostResult = document.querySelector('#li-totalCost')
+    const moneyLeftResult = document.querySelector('#li-moneyLeft')
 
     const loanSum = housePrice - deposit;
-    const amortizationPerMonth = (amortizationPerYear * loanSum) / 12;
-    const interestPerMonth = amortizationPerMonth * interest;
-    const totalCostsPerMonth = amortizationPerMonth + interestPerMonth + opCosts + otherCosts;
-    const moneyLeft = income - totalCostsPerMonth;
+    const amortizationPerMonth = (amortizationPerYear * loanSum) / 12
+    const interestPerMonth = amortizationPerMonth * interest
+    const totalCostsPerMonth = amortizationPerMonth + interestPerMonth + opCosts + otherCosts
+    const moneyLeft = income - totalCostsPerMonth
 
-    totalCostResult.textContent = totalCostsPerMonth.toFixed(1) + ' SEK';
-    moneyLeftResult.textContent = moneyLeft.toFixed(1) + ' SEK';
-});
+    totalCostResult.textContent = totalCostsPerMonth.toFixed(1) + ' SEK'
+    moneyLeftResult.textContent = moneyLeft.toFixed(1) + ' SEK'
+}
+
+function validateInputFields() {
+    const errorMessage = "Du måste fylla i ett värde"
+    const inputFields = document.querySelectorAll('input')
+    let results = []
+
+    for (input of inputFields) {
+        if (input.value === '') {
+            input.classList.add('warning')
+            input.previousElementSibling.textContent = errorMessage
+            input = false
+            results.push(input)
+        } else {
+            input.classList.remove('warning')
+            input.previousElementSibling.textContent = ''
+        }
+    }
+
+    if (!results.includes(false)) {
+        getResult()
+    }
+}
